15517feaac73226e8c11d22073e6ed0cd661b84e
p568912 <62595843+p568912@users.noreply.github.com>
2022-08-08 22:40:57 +0800

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,10 +10,10 @@ cd /home/ec2-user/express-app
 # export NVM_DIR="$HOME/.nvm"	
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-source /home/ec2-user/.bash_profile
+# source /home/ec2-user/.bash_profile
 
 #install node modules
-npm install
+npm install .
 
 #start our node app in the background
 node app.js > app.out.log 2> app.err.log < /dev/null & 
