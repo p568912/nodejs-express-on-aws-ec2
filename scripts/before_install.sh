cb7ccb83e8b432f6d3413a93124a8697f18f5de4
p568912 <62595843+p568912@users.noreply.github.com>
2022-08-08 23:00:27 +0800

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,10 +2,11 @@
 
 #download node and npm
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-. ~/.nvm/nvm.sh
-# nvm install node
-nvm install 16
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+# . ~/.nvm/nvm.sh
+# # nvm install node
+# nvm install 16
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
