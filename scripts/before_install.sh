6219f3fd1e0dc1db589f344b8fc248420e863d0c
p568912 <62595843+p568912@users.noreply.github.com>
2022-08-09 13:59:47 +0800

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #download node and npm
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 # . ~/.nvm/nvm.sh
 # # nvm install node
-# nvm install 16
-sudo wget https://nodejs.org/dist/latest-v16.x/node-v16.16.0-linux-x64.tar.gz
-tar zvxf node-v16.16.0-linux-x64.tar.gz
+nvm install 16
+# sudo wget https://nodejs.org/dist/latest-v16.x/node-v16.16.0-linux-x64.tar.gz
+# tar zvxf node-v16.16.0-linux-x64.tar.gz
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
