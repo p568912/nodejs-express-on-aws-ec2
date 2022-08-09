aeb96f654daced1c98db43b7ab9d3130d4db78b5
p568912 <62595843+p568912@users.noreply.github.com>
2022-08-09 13:27:20 +0800

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,8 +10,11 @@ cd /home/ec2-user/express-app
 # export NVM_DIR="$HOME/.nvm"	
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+sudo nano ~/.bashrc
+export PATH=$PATH:/home/ec2-user/node-v16.16.0-linux-x64/bin
 source /home/ec2-user/.bash_rc
 # export PATH=/home/ec2-user/.nvm/versions/node/v16.16.0/lib/node_modules/npm:$PATH
+
 
 #install node modules
 npm install .
