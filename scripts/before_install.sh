8ea6d06abc331428a08a6976eda88828866b8c1f
p568912 <62595843+p568912@users.noreply.github.com>
2022-08-09 14:12:27 +0800

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,7 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-# . ~/.nvm/nvm.sh
+. ~/.nvm/nvm.sh
 # # nvm install node
 nvm install 16
 # sudo wget https://nodejs.org/dist/latest-v16.x/node-v16.16.0-linux-x64.tar.gz
