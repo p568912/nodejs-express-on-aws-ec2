21efa70ef29dd56d72ad0a7e62bf0178219feb17
p568912 <62595843+p568912@users.noreply.github.com>
2022-08-22 20:21:15 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 module.exports = {
-  auth: require("./auth"),
-  fugu: require("./fugu-route"),
-  file: require("./file-route"),
-  source: require("./source-route"),
+  example: require("./example"),
+
 };
