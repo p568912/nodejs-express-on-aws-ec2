d3d523e9e4c3badd2843aa236133a3d74acf8531
yujing <poirot229@gmail.com>
2022-08-22 22:13:46 +0800

    add command

--- app.js
+++ app.js
@@ -1,40 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 let con = mysql.createConnection({
   host: "localhost",
   user: "test",
-  password: "Phx.202207"
+  password: "Phx.202207",
 });
 
-app.get('/', (req, res) => {
-  let sql_cmd="";
-  con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        con.query(sql_cmd, function (err, result) {
-            if (err) {
-              throw err;
-              console.log("Error: " + err);
-              res.send('<h1>Acess DB failed</h1> <h4>{result}</h4>');
-
-            }
-            console.log("Result: " + result);
-            res.send('<h1>Express Demo App</h1> <h4>Message: {result}</h4> ');
-
-        });
-   });
-
-})
+app.get("/", (req, res) => {
+  let sql_cmd = "select * from user;";
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query(sql_cmd, function (err, result) {
+      if (err) {
+        throw err;
+        console.log("Error: " + err);
+        res.send("<h1>Acess DB failed</h1> <h4>{result}</h4>");
+      }
+      console.log("Result: " + result);
+      res.send("<h1>Express Demo App</h1> <h4>Message: {result}</h4> ");
+    });
+  });
+});
 
 // app.get('/sqlDemo', (req, res) => {
-  
+
 // })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`Demo app is up and listening to port: ${port}`);
-})
- 
+});
